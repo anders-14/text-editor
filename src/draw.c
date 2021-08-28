a76d48145362a73a69e1145082c8390518de1235
Anders Fimreite <anders.fimreite@gmail.com>
2021-08-28 18:03:15 +0200

    clean up some code

--- src/draw.c
+++ src/draw.c
@@ -33,8 +33,7 @@ void drawPrompt(editorConfig *E, abuf *ab)
 
 void drawRows(editorConfig *E, abuf *ab)
 {
-  int i;
-  for (i = 0; i < E->editorRows; i++) {
+  for (int i = 0; i < E->editorRows; i++) {
     if (i >= E->numRows) {
       abAppend(ab, "~", 1);
     } else {
